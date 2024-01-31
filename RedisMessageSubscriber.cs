ca6b4cbcf4c7b2638b38fa9e8aac70e091144ed8
Waqas Ahmed Rehmani <waqasahmed.rehmani@uworx.co.uk>
2024-01-31 23:32:05 +0500

    Add project files.

---
+++ RedisMessageSubscriber.cs
@@ -0,0 +1,29 @@
+﻿
+using StackExchange.Redis;
+
+namespace BankTrackingSystem
+{
+    public class RedisMessageSubscriber : BackgroundService
+    {
+        private readonly ILogger<RedisMessageSubscriber> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly ConnectionMultiplexer _connectionMultiplexer;
+        private static readonly RedisChannel _channel = RedisChannel.Pattern("bank-account-status-changed-channel");
+
+        public RedisMessageSubscriber(IConfiguration configuration, ILogger<RedisMessageSubscriber> logger)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _connectionMultiplexer = ConnectionMultiplexer.Connect("localhost:6379,password=redispw,user=default");
+            //_connectionMultiplexer = ConnectionMultiplexer.Connect(_configuration.GetConnectionString("SQLConnectionString"));
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var subscriber = _connectionMultiplexer.GetSubscriber();
+            await subscriber.SubscribeAsync(_channel, (channel, message) =>
+            {
+                _logger.LogInformation("Received message: {Channel} {Message}", channel, message);
+            });
+        }
+    }
+}
