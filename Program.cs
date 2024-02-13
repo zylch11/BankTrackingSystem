e951ddd30bf59bb601deebddd40ad19ca61cd7a9
Waqas Ahmed Rehmani <waqasahmed.rehmani@uworx.co.uk>
2024-02-13 16:02:28 +0500

    apply migrations on startup

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using BankTrackingSystem;
 using BankTrackingSystem.Data;
 using BankTrackingSystem.Models;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +11,10 @@ builder.Services.AddDbContext<DatabaseContext>();
 builder.Services.AddHostedService<RedisMessageSubscriber>();
 builder.Services.AddScoped<IApplicantMessagesRespository, ApplicantMessagesRespository>();
 
-
 var app = builder.Build();
+using var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+await context.Database.MigrateAsync();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -32,4 +35,4 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+await app.RunAsync();
